896d932d18bc3758bc7f4f2fb6ae0335e715ffed
Christiaan Janssen <113596768+chrisjan-work@users.noreply.github.com>
2025-04-07 08:13:41 +0200

    refactor bottom sheet code

--- app/src/main/java/com/lairofpixies/choppity/ui/MainScreenScaffold.kt
+++ app/src/main/java/com/lairofpixies/choppity/ui/MainScreenScaffold.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetValue
@@ -30,9 +31,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.lairofpixies.choppity.logic.MainViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 
@@ -55,86 +60,10 @@ fun MainScreenScaffold(
         WindowInsets.navigationBars.getBottom(LocalDensity.current).toDp()
     }
 
-    var optionsChoice by remember { mutableStateOf(OptionCategories.ASPECT_RATIO) }
-
     BottomSheetScaffold(
         topBar = topBar,
         scaffoldState = scaffoldState,
-        sheetContent = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                // chooser
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    val params = viewModel.processParams.collectAsState().value
-                    Button(onClick = {
-                        optionsChoice = OptionCategories.ASPECT_RATIO
-                        scope.launch { scaffoldState.bottomSheetState.expand() }
-                    }) {
-                        params.aspectRatio.let {
-                            val text = if (it.width <= 0f || it.height <= 0f) {
-                                "Orig"
-                            } else {
-                                "${it.width.toInt()}:${it.height.toInt()}"
-                            }
-                            Text(text)
-                        }
-                    }
-                    Button(onClick = {
-                        optionsChoice = OptionCategories.FILL_COLOR
-                        scope.launch { scaffoldState.bottomSheetState.expand() }
-                    }) {
-                        Box(
-                            Modifier
-                                .size(36.dp, 24.dp)
-                                .background(params.bgColor)
-                        )
-                    }
-                    Button(onClick = {
-                        optionsChoice = OptionCategories.SECTION_COUNT
-                        scope.launch { scaffoldState.bottomSheetState.expand() }
-                    }) {
-                        Text(text = params.sectionCount.toString())
-                    }
-                }
-
-                Spacer(Modifier.height(24.dp))
-
-                // button views
-                val optionButtonsAlpha =
-                    if (scaffoldState.bottomSheetState.currentValue != SheetValue.PartiallyExpanded) {
-                        1.0f
-                    } else {
-                        0.0f
-                    }
-                when (optionsChoice) {
-                    OptionCategories.ASPECT_RATIO -> AspectRatioRow(
-                        modifier = Modifier.alpha(optionButtonsAlpha),
-                        setAspectRatio = { aspectRatio ->
-                            viewModel.setAspectRatio(
-                                aspectRatio
-                            )
-                        }
-                    )
-
-                    OptionCategories.FILL_COLOR -> ColorRow(
-                        modifier = Modifier.alpha(optionButtonsAlpha),
-                        setColor = { color -> viewModel.setRenderColor(color) }
-                    )
-
-                    OptionCategories.SECTION_COUNT -> SectionRow(
-                        modifier = Modifier.alpha(optionButtonsAlpha),
-                        setSections = { sections -> viewModel.setSections(sections) }
-                    )
-                }
-                Spacer(Modifier.height(extraBottomPeek))
-            }
-        },
+        sheetContent = { BottomSheet(viewModel, scope, scaffoldState, extraBottomPeek) },
         sheetPeekHeight = extraBottomPeek + 80.dp,
         sheetSwipeEnabled = true,
         sheetDragHandle = { /* Empty drag handle */ },
@@ -142,4 +71,113 @@ fun MainScreenScaffold(
         modifier = modifier,
         content = content
     )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun BottomSheet(
+    viewModel: MainViewModel,
+    scope: CoroutineScope,
+    scaffoldState: BottomSheetScaffoldState,
+    extraBottomPeek: Dp
+) {
+    var optionCategory by remember { mutableStateOf(OptionCategories.ASPECT_RATIO) }
+    val selectCategory: (OptionCategories) -> Unit = { category ->
+        optionCategory = category
+        scope.launch { scaffoldState.bottomSheetState.expand() }
+    }
+    val optionButtonsAlpha =
+        if (scaffoldState.bottomSheetState.currentValue != SheetValue.PartiallyExpanded) {
+            1.0f
+        } else {
+            0.0f
+        }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        // chooser
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            val params = viewModel.processParams.collectAsState().value
+            AspectRatioCategoryButton(params.aspectRatio, selectCategory)
+            FillColorCategoryButton(params.bgColor, selectCategory)
+            SectionCountCategoryButton(params.sectionCount, selectCategory)
+        }
+
+        Spacer(Modifier.height(24.dp))
+
+        // button views
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .alpha(optionButtonsAlpha)
+        ) {
+            when (optionCategory) {
+                OptionCategories.ASPECT_RATIO -> AspectRatioRow(
+                    setAspectRatio = { aspectRatio -> viewModel.setAspectRatio(aspectRatio) }
+                )
+
+                OptionCategories.FILL_COLOR -> ColorRow(
+                    setColor = { color -> viewModel.setRenderColor(color) }
+                )
+
+                OptionCategories.SECTION_COUNT -> SectionRow(
+                    setSections = { sections -> viewModel.setSections(sections) }
+                )
+            }
+        }
+        Spacer(Modifier.height(extraBottomPeek))
+    }
+}
+
+@Composable
+fun AspectRatioCategoryButton(
+    aspectRatio: Size,
+    selectCategory: (OptionCategories) -> Unit,
+) {
+    Button(onClick = {
+        selectCategory(OptionCategories.ASPECT_RATIO)
+    }) {
+        aspectRatio.let {
+            val text = if (it.width <= 0f || it.height <= 0f) {
+                "Orig"
+            } else {
+                "${it.width.toInt()}:${it.height.toInt()}"
+            }
+            Text(text)
+        }
+    }
+}
+
+@Composable
+fun FillColorCategoryButton(
+    fillColor: Color,
+    selectCategory: (OptionCategories) -> Unit
+) {
+    Button(onClick = {
+        selectCategory(OptionCategories.FILL_COLOR)
+    }) {
+        Box(
+            Modifier
+                .size(36.dp, 24.dp)
+                .background(fillColor)
+        )
+    }
+}
+
+@Composable
+fun SectionCountCategoryButton(
+    sectionCount: Int,
+    selectCategory: (OptionCategories) -> Unit
+) {
+    Button(onClick = {
+        selectCategory(OptionCategories.SECTION_COUNT)
+    }) {
+        Text(text = sectionCount.toString())
+    }
 }
