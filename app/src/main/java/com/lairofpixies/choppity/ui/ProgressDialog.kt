5f97945bad64b6be79013aa542239138537229f4
Christiaan Janssen <113596768+chrisjan-work@users.noreply.github.com>
2025-04-12 10:43:33 +0200

    changes in progress dialog UI

--- app/src/main/java/com/lairofpixies/choppity/ui/ProgressDialog.kt
+++ app/src/main/java/com/lairofpixies/choppity/ui/ProgressDialog.kt
@@ -3,18 +3,24 @@ package com.lairofpixies.choppity.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.lairofpixies.choppity.ui.theme.ChoppityTheme
 
 @Composable
 fun ProgressDialog(
@@ -31,16 +37,32 @@ fun ProgressDialog(
         Box(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(8.dp))
-                .sizeIn(320.dp, 320.dp)
+                .sizeIn(320.dp, 120.dp)
                 .border(1.dp, MaterialTheme.colorScheme.onSurface, RoundedCornerShape(8.dp))
                 .padding(24.dp),
             contentAlignment = Alignment.Center
         ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(48.dp),
-                strokeWidth = 4.dp,
-                color = MaterialTheme.colorScheme.primary
-            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(48.dp),
+                    strokeWidth = 4.dp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Spacer(Modifier.size(24.dp))
+                Text(
+                    text = "Saving...",
+                    fontSize = 28.sp
+                )
+            }
+
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProgressDialog() {
+    ChoppityTheme {
+        ProgressDialog {}
     }
 }
